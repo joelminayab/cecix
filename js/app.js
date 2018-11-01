882217986fdc87ed6e3f264302c16ea107c6aa2b
Joelminayab <joelminayab@gmail.com>
2018-10-31 19:45:28 -0500

    cambios del dia 31 oct ----- mejoras de la web

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$(document).ready(main);
+var contador = 1;
+function main () {
+	// $("nav").css("display" , "none");
+	$(".menu_bar").click(function(){
+		if (contador == 1) {
+			$("nav").animate({
+				top: "-100%"
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$("nav").animate({
+				top: "1%"
+			});
+		}
+	});
+	$(window).scroll(function(){
+	    window_y = $(window).scrollTop();
+	    scroll_critical = $("#header-menu").height();
+	    if (window_y > scroll_critical) {
+	       $("#header-menu").css({
+			  	"background": "#fff"
+			});
+	       $(".lala").css({
+	       		"color" : "gray"
+	       });
+	    } else {
+	    	$("#header-menu").css({
+	    		"background-color" : "transparent"
+	    	});
+	    	 $(".lala").css({
+	       		"color" : "#fff"
+	       });
+	    }
+	});
+}
+
