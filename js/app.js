ad08911f03d5966eb55af75b749eabe7564d596e
joelminayab <joelminayab@gmail.com>
2018-11-05 21:54:16 -0500

    ultimos cambios

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function main () {
 	    scroll_critical = $("#header-menu").height();
 	    if (window_y > scroll_critical) {
 	       $("#header-menu").css({
-				  "background": "transparent"
+				  "background": "white"
 			});
 	       $(".lala").css({
 	       		"color" : "gray"
