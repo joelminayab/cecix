aec8641af6af38e0a31c6fcee41c8134b8ec0dc4
joelminayab <joelminayab@gmail.com>
2018-11-06 23:18:11 -0500

    XXX

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function main () {
 	    scroll_critical = $("#header-menu").height();
 	    if (window_y > scroll_critical) {
 	       $("#header-menu").css({
-				  "background": "white"
+				  "background": "transparent"
 			});
 	       $(".lala").css({
 	       		"color" : "gray"
